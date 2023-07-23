8e95d6a9d7879b86a2a1854fe26f43a67838e6af
mfinity20 <139000916+mfinity20@users.noreply.github.com>
2023-07-23 23:55:46 +0200

    licence update

--- push_git.cmd
+++ push_git.cmd
@@ -1,4 +1,4 @@
-rem	cd C:\Program Files (x86)\mfinity20_instance_1\MQL4\Files\Mfinity\Licence
+rem	cd C:\MT4\Account_1\MQL4\Files\Mfinity\Licence
 rem	git checkout dev
 git add .
 git commit -am "licence update"
