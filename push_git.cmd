599a002654301e5b82d8cc767cb7733342e526d2
mfinity20 <support@mfinity20.com>
2023-12-16 18:12:02 +0100

    licence update

--- push_git.cmd
+++ push_git.cmd
@@ -1,6 +1,10 @@
 rem	cd C:\MT4\Account_1\MQL4\Files\Mfinity\Licence
-rem	git checkout dev
+
+rem	git local einrichten:
+rem	git clone https://github.com/mfinity20/Licence
 rem	git config --global user.email "support@mfinity20.com"
+
+git pull
 
 git add .
 git commit -am "licence update"
